787bd7ee33c06a7a729e05152845dbe8dd14f169
Nick Tsai <nicktsai1026@gmail.com>
2023-04-18 23:05:34 -0400

    update homepage layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Metadata from "@/data/meta.json";
 import Image from 'next/image';
+import HomepageData from '@/data/homepage.json';
 
 interface Meta {
     "page": string,
@@ -18,19 +19,29 @@ export const metadata = {
 
 const HomePage = () => {
     return (
-        <div className='container mx-auto'>
-            <div className="relative min-h-4/5">
-                <div className="flex items-center justify-center min-h-4/5">
-                    <h1 className="z-10">Welcome to the pass map</h1>
+        <>
+            <div className="min-h-4/5 bg-homepage-cover bg-cover bg-center flex justify-center items-center">
+                <div className="p-5 text-center bg-gray-100 dark:bg-gray-700 rounded-lg bg-opacity-50 dark:bg-opacity-50">
+                    <h1 className="mb-4 text-xl">{HomepageData.cover.heading}</h1>
+                    <h1 className="text-2xl">{HomepageData.cover.greeting}</h1>
                 </div>
-                <Image
-                    src="/homepage_cover.jpg"
-                    alt="Homepage cover"
-                    fill
-                    priority
-                />
             </div>
-        </div>
+            <div className="container mx-auto max-w-5xl px-6 pt-16 grid grid-cols-2 gap-12">
+                <div className="intro">
+                    <h1 className="text-2xl font-semibold">{HomepageData.intro.title}</h1>
+                    <span className="text-xs">{HomepageData.intro.description}</span>
+                    <p className="text-base font-normal">{HomepageData.intro.content}</p>
+                </div>
+                <div className="relative">
+                    <Image
+                        src={HomepageData.intro.image}
+                        alt={HomepageData.intro.title}
+                        fill
+                        priority
+                    />
+                </div>
+            </div>
+        </>
     )
 }
 
