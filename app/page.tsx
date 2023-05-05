3c0e6ea82fedadcfa950d7ce62a6c19ad4cde3fa
Nick Tsai <nicktsai1026@gmail.com>
2023-05-04 23:09:46 -0400

    cover layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,16 @@
-import Metadata from "@/data/meta.json";
-import Image from 'next/image';
+import Meta from "@/data/meta.json";
 import HomepageData from '@/data/homepage.json';
+import MuseumCulturalVenues from '@/data/museum_cultural_venues.json';
+import { PrimaryButton  } from "@/components/MUI";
 
-interface Meta {
+interface MetaInterface {
     "page": string,
     "title": string,
     "keywords": string,
     "description": string,
 }
 
-const siteMeta: Meta = Metadata.find(meta => meta.page == 'home') || Metadata[0];
+const siteMeta: MetaInterface = Meta.find(meta => meta.page == 'home') || Meta[0];
 
 export const metadata = {
     title: siteMeta.title,
@@ -20,26 +21,27 @@ export const metadata = {
 const HomePage = () => {
     return (
         <>
-            <div className="min-h-4/5 bg-homepage-cover bg-cover bg-center flex justify-center items-center">
-                <div className="p-5 m-5 text-center bg-gray-100 dark:bg-gray-700 rounded-lg bg-opacity-50 dark:bg-opacity-50">
-                    <h1 className="mb-4 text-xl">{HomepageData.cover.heading}</h1>
-                    <h1 className="text-2xl">{HomepageData.cover.greeting}</h1>
+            <div className="min-h-9/10 bg-light-museum-cover dark:bg-dark-museum-cover bg-cover bg-center flex justify-center items-center">
+                <div className="bg-opacity-30 dark:bg-opacity-80 bg-gray-950 min-h-9/10 w-full flex items-center">
+                    <div className="container mx-auto max-w-5xl p-12 lg:p-16 grid grid-cols-1 gap-10 sm:grid-cols-2">
+                        <div className="flex flex-col justify-around text-center sm:text-start">
+                            <h1 className="text-2xl md:text-4xl lg:text-5xl leading-relaxed md:leading-relaxed lg:leading-relaxed">
+                                {HomepageData.greeting.text}
+                            </h1>
+                            <div className="operation mt-6 sm:mt-0">
+                                <PrimaryButton link={HomepageData.greeting.button.href} text={HomepageData.greeting.button.text} /> 
+                            </div>
+                        </div>
+                        <div className="min-h-2/5 hidden sm:block px-10 bg-contain bg-no-repeat bg-center
+                            bg-light-museum-image dark:bg-dark-museum-image">
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div className="container mx-auto max-w-5xl px-5 pt-16 grid grid-cols-1 gap-10 sm:grid-cols-2">
-                <div className="intro">
-                    <h1 className="text-2xl font-semibold">{HomepageData.intro.title}</h1>
-                    <span className="text-xs">{HomepageData.intro.description}</span>
-                    <p className="text-base font-normal">{HomepageData.intro.content}</p>
-                </div>
-                <div className="relative min-h-2/5">
-                    <Image
-                        src={HomepageData.intro.image}
-                        alt={HomepageData.intro.title}
-                        fill
-                        priority
-                    />
-                </div>
+            <div className="">
+                {MuseumCulturalVenues.map((museum) => (
+                    <h1 key={museum.id}>{museum.name}</h1>
+                ))}
             </div>
         </>
     )
