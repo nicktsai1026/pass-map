06b378a0e864aeffa7eea8a3d20cd35d97060497
Nick Tsai <nicktsai1026@gmail.com>
2023-05-04 23:09:16 -0400

    mui custom theme

---
+++ components/MUI.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+
+const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#FD8964'
+      }
+    },
+});
+
+export const PrimaryButton = ({ 
+    link, text 
+}: {
+    link: string;
+    text: string;
+}) => {
+    const router = useRouter();
+    return (
+        <ThemeProvider theme={theme}>
+            <Button variant="outlined" color="primary"
+                sx={{
+                    textTransform: 'none',
+                    letterSpacing: '2px',
+                    fontWeight: 'bold',
+                    padding: '5px 25px'
+                }}
+                onClick={() => router.push(link)}
+            >
+                {text}
+            </Button>
+        </ThemeProvider>
+    );
+};
