4d1e602db068784d97b7246d30bbe15cc42d84ab
Nick Tsai <nicktsai1026@gmail.com>
2023-06-11 20:53:39 -0400

    display selected museum page

--- app/museum-arts-passes/[id]/page.tsx
+++ app/museum-arts-passes/[id]/page.tsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import axios from 'axios';
+import Libraries from '@/data/libraries.json';
+import Attractions from '@/data/museum_cultural_venues.json';
+import AddressAutocomplete from '@/components/addressAutocomplete';
 
-const page = () => {
+interface attractionsProps {
+  params: {
+    id: string
+  }
+}
+
+const AttractionsPage = ({
+  params
+}: attractionsProps) => {
+  let attraction = Attractions.find(attraction => attraction.slug === params.id);
+  let attractionLibraries = Libraries.filter(library => {
+    if (attraction && library.events.includes(attraction.id)) return library;
+  });
+
   return (
-    <div>page</div>
+    <div className="mt-16 p-10">
+      <p>Attractions: {params.id}</p>
+      <AddressAutocomplete />
+      { attraction && attractionLibraries.map(library => (
+        <div key={library.slug}>
+          <p>{library.name}</p>
+          <img src={library.image} alt={library.name} />
+          <p>{library.address}</p>
+        </div>
+      ))}
+    </div>
+
   )
 }
 
-export default page
+export default AttractionsPage
